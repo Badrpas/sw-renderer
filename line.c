c8e18baa2360de33278191b9712ed4d904033dec
Vadim Samarin <dezivert@gmail.com>
2020-09-25 16:56:35 +0300

    line fix

--- line.c
+++ line.c
@@ -8,10 +8,39 @@ void line(UINT32 x0, UINT32 y0, UINT32 x1, UINT32 y1, UINT32 color) {
   UINT32 xl = x1;
   UINT32 yl = y1;
 
+  UINT32 steep = 0;
   UINT32 xlength = abs((int)xl - (int)xs);
   UINT32 ylength = abs((int)yl - (int)ys);
 
-  UINT32 steep = 0;
+  if (xlength < ylength) { // if the line is steep, we transpose the image
+    xs = y0;
+    ys = x0;
+    xl = y1;
+    yl = x1;
+    steep = 1;
+  }
+
+  if (xs>xl) { // make it left−to−right
+    UINT32 t = xs;
+    xs = xl;
+    xl = t;
+    t = ys;
+    ys = yl;
+    yl = t;
+  }
+  for (int x=xs; x<=xl; x++) {
+    float t = (x-xs)/(float)(xl-xs);
+    int y = ys*(1.-t) + yl*t;
+    if (steep) {
+      setPixel(y, x, color);
+    } else {
+      setPixel(x, y, color);
+    }
+  }
+  return;
+
+  // =======
+
   if (xlength < ylength) {
     xs = y0;
     ys = x0;
@@ -20,7 +49,7 @@ void line(UINT32 x0, UINT32 y0, UINT32 x1, UINT32 y1, UINT32 color) {
     steep = 1;
   }
 
-  if (x0 > x1) {
+  if (xs > xl) {
     xs = x1;
     ys = y1;
     xl = x0;
